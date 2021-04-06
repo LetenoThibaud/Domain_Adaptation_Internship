2520db046ab609db79ceb36885127b88e663b4d2
LetenoThibaud <letenothibaudl@gmail.com>
2021-04-06 16:07:57 +0200

    tests of DA on the four datasets with OT

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import gzip
 import pickle
 import random
 import sys
+import ot
 import time
 import pandas as pd
 import numpy as np
@@ -25,6 +26,11 @@ def loadCsv(path):
 
 
 def data_recovery(dataset):
+    # Depending on the value on the 8th columns, the label is attributed
+    # then we can have three differents datasets :
+    # - abalone8
+    # - abalone17
+    # - abalone20
     if dataset in ['abalone8', 'abalone17', 'abalone20']:
         data = pd.read_csv("datasets/abalone.data", header=None)
         data = pd.get_dummies(data, dtype=float)
@@ -44,8 +50,8 @@ def data_recovery(dataset):
         y[y == 4] = 1
     return X, y
 
-
-def listP(dic):  # Create grid of parameters given parameters ranges
+# Create grid of parameters given parameters ranges
+def listP(dic):
     params = list(dic.keys())
     listParam = [{params[0]: value} for value in dic[params[0]]]
     for i in range(1, len(params)):
@@ -86,6 +92,7 @@ def applyAlgo(algo, p, Xtrain, ytrain, Xtest, ytest, Xtarget, ytarget, Xclean):
         dtarget = xgb.DMatrix(Xtarget)
         dclean = xgb.DMatrix(Xclean)
         evallist = [(dtrain, 'train')]
+        # p = param
         bst = xgb.train(p, dtrain, p['num_boost_round'],
                         evallist, maximize=True,
                         early_stopping_rounds=50,
@@ -102,80 +109,138 @@ def applyAlgo(algo, p, Xtrain, ytrain, Xtest, ytest, Xtarget, ytarget, Xclean):
             average_precision_score(ytarget, rankTarget)*100)
 
 
-listParams = {
-              "XGBoost": listP(
-                    {'max_depth': range(1, 6),
-                     #'eta': [10**(-i) for i in range(1, 5)],
-                     #'subsample': np.arange(0.1, 1, 0.1),
-                     #'colsample_bytree': np.arange(0.1, 1, 0.1),
-                     #'gamma': range(0, 21),
-                     'num_boost_round': range(100, 1001, 100)
-                     })
-              }
+def ot_adaptation(X_source, y_source, X_target):
+    #transport = ot.da.EMDTransport()
+    reg_entropy = 0.05
+    reg_classes = 0.01
 
-nbFoldValid = 5
-seed = 1
-if len(sys.argv) == 2:
-    seed = int(sys.argv[1])
+    # doc : Domain Adaptation OT method based on sinkhorn algorithm + LpL1 class regularization.
+    # LpL1 : class-label based regularizer built upon an lp − l1 norm (Courty et al., 2016)
+    transport = ot.da.SinkhornLpl1Transport(reg_e=reg_entropy, reg_cl=reg_classes, norm="median")
+    transport.fit(Xs=X_source, ys=y_source, Xt=X_target)
+    x_source_adapted = transport.transform(Xs=X_source)
+    return x_source_adapted
 
-results = {}
-for dataset in ['abalone20', 'abalone17', 'satimage', 'abalone8']:
-    X, y = data_recovery(dataset)
-    pctPos = 100*len(y[y == 1])/len(y)
-    dataset = "{:05.2f}%".format(pctPos) + " " + dataset
-    print(dataset)
-    np.random.seed(seed)
-    random.seed(seed)
 
-    Xsource, Xtarget, ysource, ytarget = train_test_split(X, y, shuffle=True,
-                                                          stratify=y,
-                                                          test_size=0.51)
-    Xclean = Xtarget.copy()
-    for feat, coef in [(2, 0.1), (3, 10), (4, 0)]:
-        if coef != 0:
-            Xtarget[:, feat] = Xtarget[:, feat]*coef
-        else:
-            Xtarget[np.random.choice(len(Xtarget), int(len(Xtarget)/2)),
-                    feat] = 0
-    Xtrain, Xtest, ytrain, ytest = train_test_split(Xsource, ysource,
-                                                    shuffle=True,
-                                                    stratify=ysource,
-                                                    test_size=0.3)
-    skf = StratifiedKFold(n_splits=nbFoldValid, shuffle=True)
-    foldsTrainValid = list(skf.split(Xtrain, ytrain))
-    results[dataset] = {}
-    for algo in listParams.keys():
-        start = time.time()
-        if len(listParams[algo]) > 1:  # Cross validation
-            validParam = []
-            for param in listParams[algo]:
-                valid = []
-                for iFoldVal in range(nbFoldValid):
-                    fTrain, fValid = foldsTrainValid[iFoldVal]
-                    valid.append(applyAlgo(algo, param,
-                                           Xtrain[fTrain], ytrain[fTrain],
-                                           Xtrain[fValid], ytrain[fValid],
-                                           Xtarget, ytarget, Xclean)[1])
-                validParam.append(np.mean(valid))
-            param = listParams[algo][np.argmax(validParam)]
-        else:  # No cross-validation
-            param = listParams[algo][0]
-        apTrain, apTest, apClean, apTarget = applyAlgo(algo, param,
-                                                       Xtrain, ytrain,
-                                                       Xtest, ytest,
-                                                       Xtarget, ytarget,
-                                                       Xclean)
-        results[dataset][algo] = (apTrain, apTest, apClean, apTarget)
-        print(dataset, algo, "Train AP {:5.2f}".format(apTrain),
-              "Test AP {:5.2f}".format(apTest),
-              "Clean AP {:5.2f}".format(apClean),              
-              "Target AP {:5.2f}".format(apTarget), param,
-              "in {:6.2f}s".format(time.time()-start))
-    if not os.path.exists("results"):
-        try:
-            os.makedirs("results")
-        except:
-            pass
-    f = gzip.open(f"./results/res{seed}.pklz", "wb")
-    pickle.dump(results, f)
-    f.close()
+def print_pickle(filename):
+    print("Data saved in", filename)
+    file = gzip.open(filename, 'rb')
+    data = pickle.load(file)
+    file.close()
+    for dataset in data:
+        for algo in data.get(dataset):
+            results = data[dataset][algo]
+            print("Dataset:", dataset, "Algo:", algo, "Train AP {:5.2f}".format(results[0]),
+                  "Test AP {:5.2f}".format(results[1]),
+                  "Clean AP {:5.2f}".format(results[2]),
+                  "Target AP {:5.2f}".format(results[3]),
+                  "Parameters:", results[4])
+
+
+def main(argv, adaptation=False):
+    listParams = {
+                  "XGBoost": listP(
+                        {'max_depth': range(1, 6),
+                         #'eta': [10**(-i) for i in range(1, 5)],
+                         #'subsample': np.arange(0.1, 1, 0.1),
+                         #'colsample_bytree': np.arange(0.1, 1, 0.1),
+                         #'gamma': range(0, 21),
+                         'num_boost_round': range(100, 1001, 100)
+                         })
+                  }
+
+    nbFoldValid = 5
+    seed = 2
+    if len(argv) == 2:
+        seed = int(argv[1])
+
+    results = {}
+    for dataset in ['abalone20', 'abalone17', 'satimage', 'abalone8']:  # ['abalone8']:
+        X, y = data_recovery(dataset)
+        pctPos = 100*len(y[y == 1])/len(y)
+        dataset = "{:05.2f}%".format(pctPos) + " " + dataset
+        print(dataset)
+        np.random.seed(seed)
+        random.seed(seed)
+
+        # Split the dataset between the source and the target(s)
+        Xsource, Xtarget, ysource, ytarget = train_test_split(X, y, shuffle=True,
+                                                              stratify=y,
+                                                              test_size=0.51)
+        # Keep a clean backup of Xtarget before degradation.
+        Xclean = Xtarget.copy()
+        # for loop -> degradation of the target
+        # 3 features are deteriorated : the 2nd, the 3rd and the 4th
+        for feat, coef in [(2, 0.1), (3, 10), (4, 0)]:
+            # for features 2 and 3, their values are multiplied by a coefficient
+            # resp. 0.1 and 10
+            if coef != 0:
+                Xtarget[:, feat] = Xtarget[:, feat]*coef
+            # for feature 4, some of its values are (randomly) set to 0
+            else:
+                Xtarget[np.random.choice(len(Xtarget), int(len(Xtarget)/2)),
+                        feat] = 0
+
+        # Transport source with OT
+        if adaptation:
+            Xsource = ot_adaptation(Xsource, ysource, Xtarget)
+
+        # From the source, training and test set are created
+        Xtrain, Xtest, ytrain, ytest = train_test_split(Xsource, ysource,
+                                                        shuffle=True,
+                                                        stratify=ysource,
+                                                        test_size=0.3)
+
+        # For cross validation
+        skf = StratifiedKFold(n_splits=nbFoldValid, shuffle=True)
+        foldsTrainValid = list(skf.split(Xtrain, ytrain))
+        results[dataset] = {}
+
+        for algo in listParams.keys():
+            start = time.time()
+            if len(listParams[algo]) > 1:  # Cross validation
+                validParam = []
+                for param in listParams[algo]:
+                    valid = []
+                    for iFoldVal in range(nbFoldValid):
+                        fTrain, fValid = foldsTrainValid[iFoldVal]
+                        valid.append(applyAlgo(algo, param,
+                                               Xtrain[fTrain], ytrain[fTrain],
+                                               Xtrain[fValid], ytrain[fValid],
+                                               Xtarget, ytarget, Xclean)[1])
+                    validParam.append(np.mean(valid))
+                param = listParams[algo][np.argmax(validParam)]
+            else:  # No cross-validation
+                param = listParams[algo][0]
+            apTrain, apTest, apClean, apTarget = applyAlgo(algo, param,
+                                                           Xtrain, ytrain,
+                                                           Xtest, ytest,
+                                                           Xtarget, ytarget,
+                                                           Xclean)
+            results[dataset][algo] = (apTrain, apTest, apClean, apTarget, param)
+            print(dataset, algo, "Train AP {:5.2f}".format(apTrain),
+                  "Test AP {:5.2f}".format(apTest),
+                  "Clean AP {:5.2f}".format(apClean),
+                  "Target AP {:5.2f}".format(apTarget), param,
+                  "in {:6.2f}s".format(time.time()-start))
+        if not os.path.exists("results"):
+            try:
+                os.makedirs("results")
+            except:
+                pass
+        f = gzip.open(f"./results/res{seed}.pklz", "wb")
+        pickle.dump(results, f)
+        f.close()
+
+
+if __name__ == '__main__':
+    # main(sys.argv, adaptation=False)
+    # main(sys.argv, adaptation=True)
+    print_pickle("results/res2.pklz")
+    print_pickle("results/res2_adapted.pklz")
+
+    '''
+    print_pickle("results/res2.pklz")
+    print_pickle("results/res2_transport.pklz")
+    print_pickle("results/res2_transportEMD.pklz")
+    '''
---
+++ results/results_test_backup.md
@@ -0,0 +1,11 @@
+**Without OT :**  
+00.62% abalone20 XGBoost Train AP 53.42 Test AP  2.26 Clean AP  2.82 Target AP  2.85 {'max_depth': 5, 'num_boost_round': 200} in 161.51s  
+01.39% abalone17 XGBoost Train AP 21.27 Test AP  4.05 Clean AP  4.34 Target AP  4.33 {'max_depth': 2, 'num_boost_round': 400} in 274.68s  
+09.73% satimage XGBoost Train AP 96.30 Test AP 66.42 Clean AP 68.67 Target AP 65.60 {'max_depth': 5, 'num_boost_round': 600} in 884.30s  
+13.60% abalone8 XGBoost Train AP 58.38 Test AP 30.38 Clean AP 27.88 Target AP 23.50 {'max_depth': 4, 'num_boost_round': 100} in 545.62s  
+
+**With OT :**  
+00.62% abalone20 XGBoost Train AP 17.68 Test AP  1.50 Clean AP  0.61 Target AP 12.15 {'max_depth': 5, 'num_boost_round': 100} in 233.96s  
+01.39% abalone17 XGBoost Train AP 20.45 Test AP  3.44 Clean AP  1.78 Target AP  2.60 {'max_depth': 2, 'num_boost_round': 200} in 298.69s  
+09.73% satimage XGBoost Train AP 77.15 Test AP 50.04 Clean AP 37.34 Target AP 38.39 {'max_depth': 5, 'num_boost_round': 100} in 1215.22s  
+13.60% abalone8 XGBoost Train AP 47.02 Test AP 32.42 Clean AP 19.71 Target AP 19.46 {'max_depth': 4, 'num_boost_round': 100} in 490.18s  
